942066da629bdbd964aeea59dbdb10602c215612
Ebubekir Tosun <tosunbekir91@gmail.com>
2025-07-01 18:43:39 +0200

    last commit

--- Dönem 1 Sinavlar/sinavlar/java/3. sinav adressVerwaltung/Kodlar yazilmis hali/AddressVerwaltung/src/adressVerwaltung/AddressBook.java
+++ Dönem 1 Sinavlar/sinavlar/java/3. sinav adressVerwaltung/Kodlar yazilmis hali/AddressVerwaltung/src/adressVerwaltung/AddressBook.java
@@ -1,11 +1,18 @@
 package adressVerwaltung;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AddressBook {
     private List<Company> companies;
     private List<Department> departments;
     private List<Person> persons;
+
+    public AddressBook() {
+        this.companies = new ArrayList<>();
+        this.departments = new ArrayList<>();
+        this.persons = new ArrayList<>();
+    }
 
     public void add(Person person) {
         persons.add(person);
@@ -20,14 +27,20 @@ public class AddressBook {
         int count = 0;
         for (Person person : persons) {
             if (person.isEmployeeOf(company)) {
-                System.out.println(person.getId() + ": " + person.getName() + "\n" +
-                        "P: " + person.getAddress() + " (" + person.getAddress().getAccessCount() + ")");
+                System.out.println(person.getId() + ": " + person.getName());
 
+                // Person adresi varsa yazdır
+                if (person.getAddress() != null) {
+                    System.out.println("P: " + person.getAddress() + " (" + person.getAddress().getAccessCount() + ")");
+                } else {
+                    System.out.println("P: -");
+                }
+
+                // Departman adresi varsa yazdır, yoksa şirket adresini yazdır
                 Department dept = person.getDepartment();
                 if (dept.getAddress() != null) {
                     System.out.println("G: " + dept.getAddress() + " (" + dept.getAddress().getAccessCount() + ")");
                 } else {
-
                     System.out.println("G: " + dept.getCompany().getAddress() + " (" + dept.getCompany().getAddress().getAccessCount() + ")");
                 }
 
@@ -36,6 +49,7 @@ public class AddressBook {
         }
         System.out.println("Anzahl gefundene Adressen: " + Address.getTotalAccessCount());
     }
+
 
     public static void print(Person person) {
         System.out.println(person.getName());
